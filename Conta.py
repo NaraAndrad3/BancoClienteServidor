9673172ab23e51a6e04dc10623645ba5eec59bf2
NaraAndrad3 <naradiasspn1967@gmail.com>
2023-02-15 21:08:40 -0300

    Documentação Conta

--- Conta.py
+++ Conta.py
@@ -57,56 +57,9 @@ class Conta:
     def historico(self,h):
         self._historico = h
    
-    def sacar(self,valor): 
-        valor = float(valor)
-        if valor <= self.saldo and valor > 0:
-            self.saldo -= valor
-            self._historico.add_transacao(f'{datetime.datetime.now()} - Saque realizado com sucesso! Valor: {valor}\n')
-            return True
-        else:
-            self._historico.add_transacao(f'{datetime.datetime.now()} - Saque não realizado!\n')
-            return False
-
-    def depositar(self,valor):
-        valor = float(valor)
-        if valor >= 0:
-            self.saldo+= valor
-            self.historico.add_transacao(f'{datetime.datetime.now()} - Deposito realizado com sucesso! Valor: {valor}\n')
-            return True
-        else:
-            self.historico.add_transacao(f'{datetime.datetime.now()} - Deposito não realizado!\n')
-            return False
-
-    def transferir(self,origem,destino, valor):
-        valor = float(valor)
-        if self.numero == origem.numero:
-            self.saldo -= valor 
-            #self._historico.add_transacao(f'{datetime.datetime.now()} - Transferência realizada para {destino.titular.nome} Valor: {valor}\n')
-            destino.saldo += valor
-            #destino.historico.add_transacao(f'{datetime.datetime.now()} - Transferência recebida de {origem.titular.nome}  Valor: {valor}\n')
-            return True
-           
-
     
     def __str__(self) -> str:
         return f'Titular: {self._titular}\nNúmero: {self._numero}\nSaldo: {self._saldo}'
-
-    def extrato(self):
-        self._historico.add_transacao(f'Extrato realizado com sucesso\n')
-        return f'{self.titular}\nNúmero: {self.numero}\nSaldo: {self.saldo}'
-       
-    
-
-
-
-'''''pessoa = Pessoa('nara','Dias','123','1234')
-conta = Conta(pessoa,133)
-
-print(conta.adicionarConta(conta))
-
-conta.depositar(100)
-conta.extrato()
-'''''
 
 
 
